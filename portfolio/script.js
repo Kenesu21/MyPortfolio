9d45edbf2a6ab92232d21958c5efe37e8fa22d2c
Kenesu21 <escabarte.kenneth@cec.edu.ph>
2024-04-04 11:20:36 +0800

    first commit

---
+++ portfolio/script.js
@@ -0,0 +1,30 @@
+ScrollReveal({ 
+    reset: true,
+    distance:'80px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('#header, .sub-title',{origin:'top'});
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu= document.getElementById("sidemenu");
+    function openmenu(){
+        sidemenu.style.right="0";
+    }
+    function closemenu(){
+        sidemenu.style.right="-200px";
+    }
